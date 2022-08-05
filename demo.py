c24f301b022162f0f664dbde8cc64dbcea40e520
Alex Yang <alyang@yext.com>
2022-08-04 22:45:37 -0400

    Fix error handling for field

--- demo.py
+++ demo.py
@@ -6,15 +6,7 @@ st.set_page_config(page_title="Rich Text Segmentation Demo")
 
 @st.experimental_memo(ttl=600)
 def get_results(URL):
-    response = requests.get(URL).json()
-
-    if response["meta"]["errors"]:
-        st.error("Could not fetch results for the selected search parameters.")
-        st.code(response, language="json")
-        st.stop()
-
-    return response["response"]["results"]
-
+    return requests.get(URL).json()
 
 st.header("Rich Text Segmentation Test")
 
@@ -27,22 +19,35 @@ FIELD = st.sidebar.text_input("Field", value="body")
 
 
 URL = f"https://liveapi.yext.com/v2/accounts/me/search/vertical/query?experienceKey={EXPERIENCE_KEY}&api_key={API_KEY}&limit=50&v=20220101&version=STAGING&locale=en&verticalKey={VERTICAL_KEY}&input="
-results = get_results(URL)
+response = get_results(URL)
+if response["meta"]["errors"]:
+    st.error("Could not fetch results for the selected search parameters.")
+    st.json(response)
+    st.stop()
+results = response["response"]["results"]
 fields = []
 offset = 50
 while results:
-    fields += [r["data"][FIELD] for r in results]
+    try:
+        fields += [r["data"][FIELD] for r in results]
+    except:
+        st.error("Could not fetch field from results.")
+        st.json(response)
+        st.stop
 
     NEW_URL = URL + f"&offset={offset}"
     offset += 50
-    results = get_results(NEW_URL)
+    response = get_results(NEW_URL)
+    if response["meta"]["errors"]:
+        st.error("Could not fetch results for the selected search parameters.")
+        st.json(response)
+        st.stop()
+    results = response["response"]["results"]
 
 selection = st.selectbox(label="", options=fields)
-print(selection)
 segments = selection.split("\n")
 segments = [segment.strip() for segment in segments]
 segments = [segment for segment in segments if segment]
-print(segments)
 
 left, right = st.columns(2)
 left.write("### Original Rich Text")
